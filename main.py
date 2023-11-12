12be657471e2ff47356880c3055a159a6113c6ff
VarunHari12 <varunhari@outlook.com>
2023-11-11 21:22:58 -0800

    Basic Speech
    added basic speech regocnition

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 from googletrans import Translator, constants
 from pprint import pprint
-
-translator = Translator()
+import speech_recognition as sr
 
 print("Welcome to the language translator!")
 
+r = sr.Recognizer()
+translator = Translator()
+
+with sr.Microphone() as source:
+    print("Please say what you want to translate: ")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+except:
+    print("I was not able to hear what you said.")
+    exit()
+    
+langs = list(constants.LANGUAGES.values())
+codes = list(constants.LANGUAGES.keys())
+langDict = {}
+# print(langs)
+
+for i in range(0,len(langs)):
+    langDict.update({langs[i]:codes[i]})
+
+# print(langDict)
+    
 
 while True:
-    language = input("What language would you like to translate to today? (Type \"help\" to see a list of supported languages)")
+    languageTarget = input("What language would you like to translate to today? (Type \"help\" to see a list of supported languages): ")
+    
 
-    if language.lower() == "help":
+    if languageTarget.lower() == "help":
         print("Total supported languages:", len(constants.LANGUAGES))
         print("Languages:")
-        pprint(constants.LANGUAGES)
+        pprint(langs)
     else:
         break
 
-text = input("Please input the text you would like to translate: ")   
+# languageSource = input("What language are you speaking in today: ")
+
 
 try:
-    translation = translator.translate(text, dest = language)
+    translation = translator.translate(text, dest = langDict[languageTarget])
 except ValueError:
     print("Invalid language. Please use the abbreviated form.")
 except:
     print("An error has occured")
 else:
     print(translation.text)
+    
